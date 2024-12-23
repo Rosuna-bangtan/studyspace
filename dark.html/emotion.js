6ad7f66d607ab95b45fda0d11ab44dd0211fd75e
Rosuna-bangtan <150305442+Rosuna-bangtan@users.noreply.github.com>
2024-12-22 23:47:50 -0500

    Delete dark.html directory

--- dark.html/emotion.js
+++
@@ -1,9 +0,0 @@
-const pulseCircle = document.getElementById('pulse-circle');
-const changeColorButton = document.getElementById('change-color');
-
-const colors = ['#007BFF', '#28A745', '#DC3545', '#FFC107', '#17A2B8'];
-
-changeColorButton.addEventListener('click', () => {
-    const randomColor = colors[Math.floor(Math.random() * colors.length)];
-    pulseCircle.style.backgroundColor = randomColor;
-});
